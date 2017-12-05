d37137c7d49d05945108069cf8aceada112e4e8f
Justin Knueppel <justinknueppel@gmail.com>
2017-12-05 15:01:16 -0500

    create file to quiz me on morse code -- add initial dictionary

---
+++ morseCode.py
@@ -0,0 +1,15 @@
+import random
+
+morse = {'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.', 'h':'....', 'i':'..', 'j':'.---', 'k':'-.-', 'l':'.-..', 'm':'--',
+'n':'-.', 'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.', 's':'...', 't':'-', 'u':'..-', 'v':'...-', 'w':'.--', 'x':'-..-', 'y':'-.--', 'z':'--..', 
+'1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.', '0':'-----'}
+
+# shuffle morse
+Collections.shuffle(morse)
+#TODO: choose randomly between key and value
+
+#TODO: print question
+#
+#TODO: check question against answer and display answer
+#
+#TODO: print final score and questions missed
