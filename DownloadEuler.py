8b140b9fbb0c62bfff710db4897ab0681e5d22f5
Justin Knueppel <justinknueppel@gmail.com>
2018-06-24 11:35:11 -0400

    Set up skeleton of document

---
+++ DownloadEuler.py
@@ -0,0 +1,10 @@
+"""Download all problems from the Project Euler site"""
+#install beautiful soup first as well as selenium
+import requests, bs4, selenium
+
+#Navigate to website
+
+#Get the problem number, URL, description
+
+#Open a new file with the problem number
+
