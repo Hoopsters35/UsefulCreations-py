604eabae8d63230d7405f3926c649f5d4e60115b
Justin Knueppel <justinknueppel@gmail.com>
2018-06-26 13:33:46 -0400

    Program to print all local git repos on the given {start_path} to {output_file}

---
+++ FindAllGitRepos.py
@@ -0,0 +1,20 @@
+"""Program to print directory paths of every directory containing a .git foldder"""
+
+import os, sys
+
+starting_directory = sys.argv[1]
+output_file_name = sys.argv[2]
+print('Starting directory: {}\nOutput file: {}'.format(starting_directory, output_file_name))
+
+
+def search_dir(start_dir, output_file):
+    for root, dirs, files in os.walk(start_dir):
+        if '.git' in dirs:
+            print('.git found in {}'.format(root))
+            output_file.write(str(root) + " ")
+            dirs[:] = []
+
+
+
+with open(output_file_name, 'w') as output_file:
+    search_dir(starting_directory, output_file)
