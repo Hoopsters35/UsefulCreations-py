dce284f23219c3bd3e4428c098ecb73ab28ce2fe
Justin Knueppel <justinknueppel@gmail.com>
2018-07-05 17:27:09 -0400

    Add a text command

--- ReminderCalls.py
+++ ReminderCalls.py
@@ -7,12 +7,13 @@ cancel_set = set(['can', 'canc', 'cancel'])
 no_answer_set = set(['n', 'no', 'no ans'])
 hung_up_set = set(['hung', 'hung up'])
 help_set = set(['h', 'help'])
+text_set = set(['t', 'text', 'txt'])
 
-commands = {"Left message": message_set, "Confirm" : confirm_set, "No answer" : no_answer_set, "Hung up" : hung_up_set, "Help" : help_set, "Cancel" : cancel_set}
+commands = {"Left message": message_set, "Confirm" : confirm_set, "No answer" : no_answer_set, "Hung up" : hung_up_set, "Help" : help_set, "Cancel" : cancel_set, "Text" : text_set}
 
-while (True):
+def getnote(command):
+
     output = datetime.now().strftime("%m/%d %I:%M %p ~ ")
-    command = input('Enter interaction type: ').lower()
     if command in message_set:
         output += "Left message, gave Chris' phone number to confirm or cancel appointment"
     elif command in confirm_set:
@@ -21,10 +22,19 @@ while (True):
         output += "No answer"
     elif command in hung_up_set:
         output += "Hung up mid-call"
+    elif command in text_set:
+        output += "Confirmation text sent"
     elif command in help_set:
         for command in commands.keys():
             print('{} : {}'.format(command, commands.get(command)))
 
     
-    pyperclip.copy(output)
-    print('Copied: ' + output)
+    return output
+
+if __name__ == '__main__':
+    while (True):
+        command = input('Enter interaction type: ').lower()
+        
+        output = getnote(command)
+        pyperclip.copy(output)
+        print('Copied: ' + output)
